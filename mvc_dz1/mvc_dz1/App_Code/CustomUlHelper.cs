a21ba6317d36cb04f68a3bbdad1cded29dbc6b9d
Ivan Dubov <ttrianon8@gmail.com>
2020-02-03 05:03:06 +0200

    project loaded

---
+++ mvc_dz1/mvc_dz1/App_Code/CustomUlHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_dz1.App_Code
+{
+    public static class CustomUlHelper
+    {
+		public static MvcHtmlString CreateUl(this HtmlHelper html, List<string> items, string name)
+		{
+			var ol = new TagBuilder("ol");
+			ol.MergeAttribute("name", name);
+            ol.AddCssClass("list-unstyled");
+
+            foreach (string item in items)
+			{
+				var li = new TagBuilder("li");
+                li.AddCssClass("d-flex flex-column");
+
+                var div = new TagBuilder("div");
+                var label = new TagBuilder("label");
+                label.SetInnerText(item);
+                var option = new TagBuilder("input");
+                option.MergeAttribute("name", "check");
+                option.MergeAttribute("value", item);
+                option.MergeAttribute("type", "checkbox");
+                var checkbox = new TagBuilder("input");
+                checkbox.MergeAttribute("type", "checkbox");
+                checkbox.MergeAttribute("style", "visibility: hidden;");
+                div.InnerHtml += option.ToString() + label.ToString() + checkbox.ToString();
+                ol.InnerHtml += div.ToString();
+            }
+			return new MvcHtmlString(ol.ToString());
+		}
+
+	}
+}
---
+++ mvc_dz1/mvc_dz1/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_dz1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Guest(string Name, string Text)
+        {
+            ViewBag.AddText = false;
+            if(!string.IsNullOrWhiteSpace(Name) && !string.IsNullOrWhiteSpace(Text))
+            {
+                ViewBag.AddText = true;
+                ViewBag.Name = Name;
+                ViewBag.Text = Text;
+            }
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Form()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Form(FormCollection form)
+        {
+            ViewBag.Person = form["Name"] + " " + form["Surname"];
+            ViewBag.Sex = form["gender"];
+            ViewBag.Languages = form["check"];
+
+
+            return View("~/Views/Home/Result.cshtml");
+        }
+    }
+}
